ac2412cde89edfa1252b1028c6099aec3d5a1aa5
April Watson <aprilrwat@gmail.com>
2021-08-25 11:52:06 -0500

    Tweaks for dropdown
    probably more coming

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,10 +5,13 @@ $(document).ready(function(){
   }
   
   showDropdown = () =>  {
+    let navHeight = $('.topnav').height();
     if ($(document).width() > 600) {
+      $('#dropdown-content').css({paddingTop: navHeight});
       $('#dropdown-content').toggleClass('show');
       $('.dropbtn').toggleClass('active');
     } else {
+        $('#dropdown-content').css({paddingTop: 0});
         $('#dropdown-content').hasClass('show')
         $('#dropdown-content').addClass('show')
         $('.main-nav a.main:not(.logo), .main-nav button').hide();
